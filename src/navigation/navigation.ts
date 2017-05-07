3d81709dba116907a6287c6d16135a5eb09e963b
Brainy Couch Studios <brainycouchmike@gmail.com>
2017-05-07 15:08:44 -0400

    Correctly set parent page in navigation component. prevent loading same page on top of self. Added todo scroll to page top instead of load-over.

--- src/navigation/navigation.ts
+++ src/navigation/navigation.ts
@@ -1,5 +1,4 @@
-import {Component, Input, ViewChild} from '@angular/core';
-// import {List, Item} from 'ionic/ionic';
+import {Component, Input, SimpleChange} from '@angular/core';
 
 import {Nav, NavController, NavParams} from 'ionic-angular';
 
@@ -26,19 +25,30 @@ export class NavigationDetailsPage {
 @Component({
   selector:    'navigation',
   templateUrl: 'navigation.html',
+  inputs: ['active']
 })
 export class Navigation {
+  private _active;
+  private _parent;
+  items = [];
+  itemsInMenu = [];
+
+  @Input()
   get active() {
     return this._active;
   }
 
   set active(value) {
-    this._active = value;
+    this._active = (value && value.trim()) || 'home';
   }
-  items = [];
-  itemsInMenu = [];
 
-  @Input('active') _active;
+  get parent() {
+    return this._parent;
+  }
+
+  set parent(value) {
+    this._parent = value;
+  }
 
   constructor(public nav: NavController) {
     this.items = [
@@ -46,7 +56,7 @@ export class Navigation {
         title: "Home",
         value: "home",
         page: HomePage,
-        active:false,
+        active:true,
         inMenu:false
       },
       {
@@ -84,8 +94,25 @@ export class Navigation {
   }
 
   openNavDetailsPage(item) {
-    if(this.active == item.value) item.active = true;
-    this.nav.push(item.page, { item: item });
+    if(this.parent!=item.value) {
+      if(this.active == item.value) item.active = true;
+      this.nav.push(item.page, { item: item });
+    } else {
+      // TODO: Scroll to page top
+    }
     return false;
+  }
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    for (let propName in changes) {
+      let changedProp = changes[propName];
+      let to = changedProp.currentValue;
+      if(propName=='active') this.parent = to;
+      // if (changedProp.isFirstChange()) {
+      //   console.log(`Initial value of ${propName} set to ${to}`);
+      // } else {
+      //   let from = JSON.stringify(changedProp.previousValue);
+      //   console.log(`${propName} changed from ${from} to ${to}`);
+      // }
+    }
   }
 }
